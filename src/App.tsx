80c43a5b9f60096480f199ece0937f7fcac13665
leyuri <leyuri97@gmail.com>
2022-08-21 16:45:17 +0900

    Use : useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
@@ -5,14 +6,14 @@ import Todo from './models/todo';
 
 function App() {
 
-  const todos = [
-    new Todo('Learn React'),
-    new Todo('Learn TypeScript'),
-    new Todo('Learn Next.js')
-  ];
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   const addTodoHander = (todoText: string) => {
-    
+    const newTodo = new Todo(todoText);
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo)
+    })
+
   }
 
   return (
